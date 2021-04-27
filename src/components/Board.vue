c4861ec98ff938b240a99777524f4d2a8a94444e
Hiroyuki Akimoto <hiroyuki.akimoto@willof.co.jp>
2021-04-27 16:56:17 +0900

    アプリ完成

---
+++ src/components/Board.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <header>
+          my Trello
+      </header>
+      <main>
+            <p class="info-line">All: {{ totalCardCount }} tasks</p>
+            <draggable :list="lists"
+                class="list-index"
+                @end="movingList"
+                >
+                <list v-for="(item, index) in lists"
+                    :key="item.id"
+                    :title="item.title"
+                    :cards="item.cards"
+                    :listIndex="index"
+                    @change="movingCard"
+                />
+            <list-add />
+            </draggable>
+      </main>
+  </div>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+import List from './List.vue'
+import ListAdd from './ListAdd'
+import { mapState } from 'vuex'
+
+export default {
+    components: {
+        draggable,
+        ListAdd,
+        List
+    },
+    computed: {
+        ...mapState([
+            'lists'
+        ]),
+        totalCardCount(){
+            return this.$store.getters.totalCardCount
+        },
+    },
+    methods: {
+        movingCard(){
+            this.$store.dispatch('updateList', { lists: this.lists })
+        },
+        movingList(){
+            this.$store.dispatch('updateList', { lists: this.lists })
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
